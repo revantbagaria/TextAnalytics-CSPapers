aa14e34ec88c929181d75d94043f0590473cbe4f
Revant Bagaria <bagariar@reed.edu>
2017-11-01 07:22:24 -0700

    latest

--- text_similarity.py
+++ text_similarity.py
@@ -1,4 +1,3 @@
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 from scipy.stats import itemfreq
 import numpy as np
 
@@ -61,28 +60,6 @@ def boc_term_vectors(word_list):
 # 	print 'Cosine distance is {}'.format(distance)
 # 	print 'Cosine similarity is {}'.format(similarity)
 # 	print '-'*40
-
-
-def build_feature_matrix(documents, feature_type,
-                         ngram_range=(1, 1), min_df=0.0, max_df=1.0):
-
-    feature_type = feature_type.lower().strip()  
-    
-    if feature_type == 'binary':
-        vectorizer = CountVectorizer(binary=True, min_df=min_df,
-                                     max_df=max_df, ngram_range=ngram_range)
-    elif feature_type == 'frequency':
-        vectorizer = CountVectorizer(binary=False, min_df=min_df,
-                                     max_df=max_df, ngram_range=ngram_range)
-    elif feature_type == 'tfidf':
-        vectorizer = TfidfVectorizer(min_df=min_df, max_df=max_df, 
-                                     ngram_range=ngram_range)
-    else:
-        raise Exception("Wrong feature type entered. Possible values: 'binary', 'frequency', 'tfidf'")
-
-    feature_matrix = vectorizer.fit_transform(documents).astype(float)
-    
-    return vectorizer, feature_matrix
     
 
 def compute_cosine_similarity(doc_features, corpus_features, top_n=3):
