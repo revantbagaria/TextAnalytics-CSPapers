9cd67650f658ea1d5f7d78d63411c9fce2a46b12
Revant Bagaria <bagariar@reed.edu>
2018-02-27 22:11:28 -0800

    still working

--- extract_from_xml.py
+++ extract_from_xml.py
@@ -1,46 +1,45 @@
 import xml.etree.ElementTree as ET
 
 
-# def extract_from_xml(path):
-# 	tree = ET.parse(path)
-# 	root1 = tree.getroot()
-# 	root = tree.getroot()
-
-# 	text_abstract = root1.find('front/article-meta/abstract/p')
-
-# 	if text_abstract is None or text_abstract.text is None:
-# 		text_abstract = ""
-
-# 	for front in root.iter('front'):
-# 		front.clear()
-
-# 	for back in root.iter('back'):
-# 		back.clear()
-
-# 	if text_abstract is None:
-# 		text_abstract = ""
-
-
-# 	text = text_abstract + ' '.join(root.itertext())
-# 	return text
-
-
 def extract_from_xml(path):
 	tree = ET.parse(path)
 	root1 = tree.getroot()
 	root = tree.getroot()
 
-	text = root1.find('front/article-meta/abstract/p').text
+	text_abstract = root1.find('front/article-meta/abstract/p')
+
+	if text_abstract is None or text_abstract.text is None:
+		text_abstract = ""
+	else:
+		text_abstract = text_abstract.text
+
 	for front in root.iter('front'):
 		front.clear()
 
 	for back in root.iter('back'):
 		back.clear()
 
-	# print(path)
-	# print(text)
-	if text is None:
-		text = ""
-	text = text + ' '.join(root.itertext())
+	text = text_abstract + ' '.join(root.itertext())
 	return text
+
+
+
+# def extract_from_xml(path):
+# 	tree = ET.parse(path)
+# 	root1 = tree.getroot()
+# 	root = tree.getroot()
+
+# 	text = root1.find('front/article-meta/abstract/p').text
+# 	for front in root.iter('front'):
+# 		front.clear()
+
+# 	for back in root.iter('back'):
+# 		back.clear()
+
+# 	# print(path)
+# 	# print(text)
+# 	if text is None:
+# 		text = ""
+# 	text = text + ' '.join(root.itertext())
+# 	return text
 	
