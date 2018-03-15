b996823b792201b8597576ab5b4e668eef39666b
Revant Bagaria <bagariar@reed.edu>
2018-03-15 10:14:41 -0700

    latest

--- display_features.py
+++ display_features.py
@@ -5,5 +5,5 @@ def display_features(features, feature_names, index_names=None):
     df = pd.DataFrame(data=features, index=index_names,
                       columns=feature_names)
     # print df
-    df.to_csv("similarity_matrix_inter_conf.csv")
+    df.to_csv("similarity_matrix_combined.csv")
 
