608776f41ba4c5dd433cf4dc4901b70d0155fb0f
Revant Bagaria <bagariar@reed.edu>
2018-03-23 01:38:17 -0700

    latest

--- generate_similarity.py
+++ generate_similarity.py
@@ -2,7 +2,6 @@ import numpy as np
 import findThePapers, generate_text_list, feature_extractor
 from generate_titles import generate_titles
 from display_features import display_features
-import matplotlib.pyplot as plt
 import csv
 
 def tfidf_stats(query_tfidf_features, titles_query):
@@ -80,10 +79,10 @@ def findSummarySimilarities(query_tfidf_features, corpus_tfidf_features, titles_
 	matrix = []
 	count = 1
 
-	# f = open("intraconf_similarity_stats.txt","a+")
+	f = open("intraconf_similarity_stats.txt","a+")
 
-	# f.write("Similarity Statistics for {}:\n".format(name))
-	# f.write("No of papers: %d\n" % len(titles_corpus))
+	f.write("Similarity Statistics for {}:\n".format(name))
+	f.write("No of papers: %d\n" % len(titles_corpus))
 
 	for index in range(len(query_tfidf_features)):
 		doc_tfidf = query_tfidf_features[index]
@@ -92,39 +91,23 @@ def findSummarySimilarities(query_tfidf_features, corpus_tfidf_features, titles_
 		matrix.append(np.array([""]*count + list(similarities)))
 		count += 1
 
-	# 	for i in range(len(indices_returned)):
-	# 		value = indices_returned[i] + index
-	# 		indices.append((titles_corpus[index], titles_corpus[value]))
+		for i in range(len(indices_returned)):
+			value = indices_returned[i] + index
+			indices.append((titles_corpus[index], titles_corpus[value]))
 
-	# 	result.extend(similarities)
+		result.extend(similarities)
 
-	# index_max = indices[np.argmax(result)]
-	# index_min = indices[np.argmin(result)]
+	index_max = indices[np.argmax(result)]
+	index_min = indices[np.argmin(result)]
 
-	# f.write("Mean: %f \n" %np.mean(result))
-	# f.write("Max: %f, %s \n" % (np.max(result), index_max,))
-	# f.write("Min: %f, %s \n" % (np.min(result), index_min,))
-	# f.write("Standard Deviation: %f \n" %np.std(result))
-	# f.write("\n")
+	f.write("Mean: %f \n" %np.mean(result))
+	f.write("Max: %f, %s \n" % (np.max(result), index_max,))
+	f.write("Min: %f, %s \n" % (np.min(result), index_min,))
+	f.write("Standard Deviation: %f \n" %np.std(result))
+	f.write("\n")
 
 	display_features(matrix, titles_corpus, titles_query)
-	# f.close()
-
-
-# def delete_empty_ones(corpus_docs_extended):
-
-# 	empty_files = ["CCGRID_17_012", "ISPASS_17_007", "Middleware_17_019", "NSDI_17_009", "NSDI_17_037", "NSDI_17_034", "NSDI_17_040", "NSDI_17_042", "NSDI_17_036", "NSDI_17_038", "NSDI_17_035", "NSDI_17_032", "NSDI_17_039", "NSDI_17_041", "NSDI_17_030", "NSDI_17_031", "NSDI_17_024", "NSDI_17_033", "ICPE_17_022"]
-
-# 	for i, each1 in enumerate(corpus_docs_extended):
-# 		if isinstance(each1, list):
-# 			for j, each2 in enumerate(each1):
-# 				if each2 in empty_files:
-# 					del each1[j]
-# 		else:
-# 			if each1 in empty_files:
-# 				del corpus_docs_extended[i]
-
-# 	return corpus_docs_extended
+	f.close()
 
 
 def generate_similarity(corpus_docs, query_docs, corpus_docs_append, query_docs_append, same, name=None):
@@ -140,7 +123,6 @@ def generate_similarity(corpus_docs, query_docs, corpus_docs_append, query_docs_
 		exit(1)
 
 	corpus_docs_extended = findThePapers.findThePapers(corpus_docs)
-	# corpus_docs_extended = delete_empty_ones(corpus_docs_extended)
 	titles_corpus = generate_titles(corpus_docs_extended)
 	norm_corpus = generate_text_list.generate_text_list(corpus_docs_extended)
 	corpus_tfidf_vectorizer, corpus_tfidf_features = feature_extractor.build_feature_matrix(norm_corpus,
@@ -148,7 +130,6 @@ def generate_similarity(corpus_docs, query_docs, corpus_docs_append, query_docs_
 														ngram_range=(1, 1), 
 														min_df=0.0, max_df=1.0)
 	query_docs_extended = findThePapers.findThePapers(query_docs)
-	# query_docs_extended = delete_empty_ones(query_docs_extended)
 	titles_query = generate_titles(query_docs_extended)
 	norm_query = generate_text_list.generate_text_list(query_docs_extended)
 	query_tfidf_features = corpus_tfidf_vectorizer.transform(norm_query)
